521f07ac56edb01430f7773dc9cf3a16bc3db426
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-14 19:20:33 -0500

    Create Database

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    price INT NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
