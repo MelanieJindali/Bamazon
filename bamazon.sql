d1d4eb7c41e273e77a03e9fa3343f7458c91263b
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-14 19:21:10 -0500

    Create mock products

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,5 @@ CREATE TABLE products(
     PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES ("T-Shirt", 25, "Clothing", 100), ("Bananas", 1, "Groceries", 30), ("Lipstick", 5, "Cosmetics", 50), ("Laptop", 500, "Electronics", 25), ("Sandals", 10, "Shoes", 60), ("Pillows", 7, "Home", 35), ("Leggings", 15, "Clothing", 75), ("Bedsheets", 45, "Home", 50), ("TV", 600, "Electronics", 50), ("Mascara", 8, "Cosmetics", 25);
