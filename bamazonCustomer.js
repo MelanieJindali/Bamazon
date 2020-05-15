3479c3e3229000750fe90276d20d93ee107ed9b4
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-14 19:56:54 -0500

    Add display products function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,24 @@
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Password1!",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    displayProducts();
+});
+
+function displayProducts() {
+    console.log("Displaying all available products... \n");
+    connection.query("SELECT * FROM products", function(err,res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
