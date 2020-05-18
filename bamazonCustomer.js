30a57abed8e1558ca87581a6113e8bdee7e9d39d
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-18 08:54:35 -0500

    Install inquierer & Start prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,15 +11,45 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    // console.log("connected as id " + connection.threadId + "\n");
     displayProducts();
 });
+
+function startOrder() {
+    inquirer
+        .prompt({
+            name: "prodID",
+            type: "input",
+            message: "Please enter the ID of the product you would like to buy."
+        }, {
+            name: "units",
+            type: "input",
+            message: "How much of the product would you like to buy?"
+        }) .then(function(answer) {
+        connection.query("SELECT * FROM products WHERE item_id", function(err, res) {
+            if (err) throw err;
+            
+            for (var i = 0; i < res.length; i++) {
+                console(res)
+                if (res[i].item_id === answer.prodID) {
+                    console.log(answer.prodID)
+                }
+                
+            }
+        })
+     })
+}
+
+function sellProd() {
+    console.log("Hi.")
+}
 
 function displayProducts() {
     console.log("Displaying all available products... \n");
     connection.query("SELECT * FROM products", function(err,res) {
         if (err) throw err;
-        console.log(res);
+        console.table(res);
         connection.end();
+        startOrder();
     });
 }
